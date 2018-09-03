4045dfd3b7227c035a265edc9899bec62a4a5ff2
yves <webmaster@opensnowmap.org>
2018-09-03 20:47:20 +0200

    First commit

---
+++ OSM_skimap/generate_report.py
@@ -0,0 +1,351 @@
+#!/usr/bin/python
+#
+#
+# Generate comparison data Openstreetmaps against skimaps
+
+import json
+import psycopg2
+import pdb
+from lxml import etree
+import os.path
+import urllib
+# array to store all skiAreas
+skiAreaArray=[]
+
+#~ object skiArea:
+"""
+    name = ''
+    id=-1
+    region3_id=-1
+    region2_id=-1
+    region1_id=-1
+    region3_name=''
+    region2_name=''
+    region1_name=''
+    lon=-1
+    lat=-1
+    closestLift=-1
+    closestDownhill=-1
+    closestNordic=-1
+    close=False
+    mid=False
+    far=False
+"""
+
+# dictionnary to store all regions3 parent regions
+regions3Dict={}
+"""
+{335: { 
+        name:Newfoundland and Labrador,
+        region2_id:
+        region2_name:,
+        region1_id:,
+        region2_id:,
+       },
+ 334:  {...},
+ ...
+"""
+
+
+# the generated structures looks like this:
+dataReport={}
+"""
+{ '1' : 
+        {
+        type: 'level1'
+        name: americas,
+        id:'1',
+        '36' : 
+            {
+                type: 'level2'
+                name: 'Canada',
+                id, '36',
+                '335': 
+                    {
+                        type: 'level3'
+                        name: Newfoundland and Labrador,
+                        id:'335',
+                        skiAreas :[
+                            {
+                                name:Marble Mountain,
+                                id:'5',
+                                region_id:335,
+                                lon:,
+                                lat:,
+                                closestLift:120,
+                                closestDownhill:35,
+                                closestNordic:15,
+                                close: true,
+                                mid: false,
+                                far: false
+                            },
+                            {...}
+                            ]
+                    },
+                    {...}
+                    
+            },
+            {...}
+            
+    },
+    {...}
+    
+}
+"""
+
+def makeJSON():
+    global skiAreaArray
+    global dataReport
+    
+    for area in skiAreaArray:
+        if area['region1_id'] not in dataReport.keys():
+            r= {}
+            r['type']='level1'
+            r['id']=area['region1_id']
+            r['name']=area['region1_name']
+            dataReport[area['region1_id']]=r
+        
+        if area['region2_id'] not in dataReport[area['region1_id']].keys():
+            r= {}
+            r['type']='level2'
+            r['id']=area['region2_id']
+            r['name']=area['region2_name']
+            dataReport[area['region1_id']][area['region2_id']]=r
+            
+        if area['region3_id'] not in dataReport[area['region1_id']][area['region2_id']].keys():
+            r= {}
+            r['type']='level3'
+            r['id']=area['region3_id']
+            r['name']=area['region3_name']
+            dataReport[area['region1_id']][area['region2_id']][area['region3_id']]=r
+            dataReport[area['region1_id']][area['region2_id']][area['region3_id']]['skiAreas']=[]
+        dataReport[area['region1_id']][area['region2_id']][area['region3_id']]['skiAreas'].append(area)
+        #~ print dataReport[area.region1_id][area.region2_id][area.region3_id]['skiAreas']
+            
+    with open('dataReport.json', 'w') as outfile:
+        json.dump(dataReport, outfile,sort_keys=True,
+                  indent=2, separators=(',', ': '))
+
+def loadSkimapData(filename): 
+    # Loads skimaps.json skiAreas in an objects array
+    
+    global skiAreaArray
+    global regions3Dict
+    
+    json_data=open(filename).read()
+    data = json.loads(json_data)
+    #~ print json.dumps(data, sort_keys=False,
+                  #~ indent=2, separators=(',', ': '))
+    for feature in data['features']:
+        area =  {}
+        area['name'] = feature['properties']['name']
+        area['id']=feature['properties']['id']
+        area['region1_id']='-1'
+        area['region2_id']='-1'
+        try: 
+            area['region3_id']=feature['properties']['region_id']
+        except:
+            area['region3_id']='-1'
+        try: 
+            area['region3_name']=feature['properties']['region']
+        except:
+            area['region3_name']=''
+        
+        area['region1_name']=''
+        area['region2_name']=''
+        try:
+            area['lon']=feature['geometry']['coordinates'][0]
+            area['lat']=feature['geometry']['coordinates'][1]
+        except:
+            area['lon']=-1
+            area['lat']=-1
+            
+        area['closestLift']=-1
+        area['closestDownhill']=-1
+        area['closestNordic']=-1
+        
+        if (area['region3_id'] not in regions3Dict):
+            regions3Dict[area['region3_id']]={}
+        skiAreaArray.append(area)
+        
+    
+    return True
+
+def getSkimapsParentRegion(fname): # OK
+    #~ https://skimap.org/Regions/view/335.xml
+    """
+    <region id="335" abbreviation="NL " level="3" type="Principal Subdivision">
+        <name>Newfoundland and Labrador</name>
+        <georeferencing lat="53.2257684357902" lng="-55.01953125" zoom="5"/>
+        <maps count="27"/>
+        <skiAreas>
+            <skiArea id="3033">Airport Nordic Ski Club Cross Country Ski Trails</skiArea>
+            <skiArea id="3034">Aurora Nordic Ski Club Cross Country Ski Trails</skiArea>
+            <skiArea id="3040">Birch Brook Cross Country Ski Trails</skiArea>
+            <skiArea id="3036">Blow Me Down Cross Country Ski Trails</skiArea>
+            <skiArea id="3035">Butterpot Cross Country Ski Trails</skiArea>
+            <skiArea id="1071">Copper Creek Mountain</skiArea>
+            <skiArea id="2649">Gander Winter Park</skiArea>
+            <skiArea id="5">Marble Mountain</skiArea>
+            <skiArea id="2">Northern Lights Ski Club</skiArea>
+            <skiArea id="3041">Pasadena Ski and Nature Park</skiArea>
+            <skiArea id="1062">Pine Tree</skiArea>
+            <skiArea id="1">Smokey Mountain Ski Club</skiArea>
+            <skiArea id="4">Snow Goose Mountain (Mont Shana)</skiArea>
+            <skiArea id="3043">Whaleback Nordic Ski Club</skiArea>
+            <skiArea id="3">White Hills</skiArea>
+        </skiAreas>
+        <parents>
+            <region id="1" level="1">Americas</region>
+            <region id="36" level="2">Canada</region>
+        </parents>
+    </region>
+    """
+    global regions3Dict
+    
+    if os.path.isfile(fname) :
+        # no need to ask again for testing
+        json_data=open(fname).read()
+        regions3Dict = json.loads(json_data) 
+        return True
+        
+    s=0
+    
+    for i in regions3Dict:
+        if i!= '-1':
+            region={}
+            url = 'https://skimap.org/Regions/view/'+str(i)+'.xml' 
+            
+            xmlFile = urllib.urlopen(url)
+            root = etree.fromstring(xmlFile.read())
+            parents = root.find('parents')
+            if parents:
+                regions=parents.findall('region')
+                for reg in regions:
+                    if (reg.get('level') == '1'):
+                        region['region1_id'] = reg.get('id')
+                        region['region1_name']=reg.text
+                        
+                    if (reg.get('level') == '2'):
+                        region['region2_id'] = reg.get('id')
+                        region['region2_name']=reg.text
+            
+            nameElement = root.find('name')
+            
+            region['name']=nameElement.text
+            
+            regions3Dict[i]=region
+            print(region)
+            s+=1
+            #~ if s> 10: break
+            
+    # no need to ask again for testing:
+    with open('skimaps_regions.json', 'w') as outfile:
+        json.dump(regions3Dict, outfile,sort_keys=False,
+                  indent=2, separators=(',', ': '))
+    return True
+    
+def fillRegions():
+    # Get parent region and fill the skiAreaArray with this data
+    global skiAreaArray
+    global regions3Dict
+    
+    for area in skiAreaArray:
+        
+        if (area['region3_id'] == '-1' or (area['region3_id'] not in regions3Dict)):
+            
+            area['region2_id']='-1'
+            area['region1_id']='-1'
+            area['region2_name']='-1'
+            area['region1_name']='-1'
+            continue
+            
+            
+        try: region=regions3Dict[area['region3_id']]
+        except : pdb.set_trace()
+        try:
+            area['region2_id']=region['region2_id']
+        except :
+            area['region2_id']='-1'
+        try:
+            area['region1_id']=region['region1_id']
+        except :
+            area['region1_id']='-1'
+        try:
+            area['region2_name']=region['region2_name']
+        except :
+            area['region2_name']='-1'
+        try:
+            area['region1_name']=region['region1_name']
+        except :
+            area['region1_name']='-1'
+        
+    return True
+
+def fillDistances():
+    global skiAreaArray
+    global regions3Dict
+    
+    con = psycopg2.connect("dbname=gis user=yves")
+    cur = con.cursor()
+    
+    for area in skiAreaArray:
+        lonlat={}
+        lonlat['lon']=area['lon']
+        lonlat['lat']=area['lat']
+        downhillDistance=-1
+        noridcDistance=-1
+        liftDistance=-1
+        
+        print lonlat
+        if (lonlat['lon'] != None and lonlat['lat'] != None):
+            cur.execute("""
+            SELECT ST_Distance(way, ST_Transform(ST_SetSRID(ST_MakePoint(%s,%s),4326),900913)) FROM planet_osm_line 
+            WHERE "piste:type" = 'downhill'
+            ORDER BY 
+             ST_Distance(way, ST_Transform(ST_SetSRID(ST_MakePoint(%s,%s),4326),900913)) ASC
+            LIMIT 1;
+            """
+            % (lonlat['lon'],lonlat['lat'],lonlat['lon'],lonlat['lat']))
+            downhillDistance = cur.fetchall()[0][0]
+            
+            cur.execute("""
+            SELECT ST_Distance(way, ST_Transform(ST_SetSRID(ST_MakePoint(%s,%s),4326),900913)) FROM planet_osm_line 
+            WHERE "piste:type" = 'nordic'
+            ORDER BY 
+             ST_Distance(way, ST_Transform(ST_SetSRID(ST_MakePoint(%s,%s),4326),900913)) ASC
+            LIMIT 1;
+            """
+            % (lonlat['lon'],lonlat['lat'],lonlat['lon'],lonlat['lat']))
+            nordicDistance = cur.fetchall()[0][0]
+            
+            cur.execute("""
+            SELECT ST_Distance(way, ST_Transform(ST_SetSRID(ST_MakePoint(%s,%s),4326),900913)) FROM planet_osm_line 
+            WHERE aerialway is not null
+            ORDER BY 
+             ST_Distance(way, ST_Transform(ST_SetSRID(ST_MakePoint(%s,%s),4326),900913)) ASC
+            LIMIT 1;
+            """
+            % (lonlat['lon'],lonlat['lat'],lonlat['lon'],lonlat['lat']))
+            liftDistance = cur.fetchall()[0][0]
+            
+            area['closestDownhill']=downhillDistance
+            area['closestNordic']=nordicDistance
+            area['closestLift']=liftDistance
+            print downhillDistance
+    
+    con.commit()
+    cur.close()
+    con.close()
+        
+        
+    return True
+    
+def main():
+    loadSkimapData('skimaps.geojson')
+    getSkimapsParentRegion('skimaps_regions.json')
+    fillRegions()
+    fillDistances()
+    makeJSON()
+
+main()
---
+++ OSM_skimap/web/skimap_osm.js
@@ -0,0 +1,329 @@
+
+var dataReport;
+var far = 10;
+var close = 5;
+
+function init() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', '../data/dataReport.json', true);
+    xhr.overrideMimeType("text/plain");
+    xhr.timeout = 5000; // time in milliseconds
+    
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4  && xhr.status === 200) {
+      resp = this.responseText;
+      console.log('received');
+      dataReport = JSON.parse(resp);
+      console.log('parsed');
+      buildHTML();
+     }
+    };
+    
+    xhr.ontimeout = function (e) {
+      alert('Timeout')
+    };
+    xhr.send(null);
+}
+function countskiAreas(regionObj)
+{
+    var cnt = 0;
+    var okPisteCnt = 0;
+    var okLiftCnt = 0;
+    if (typeof(regionObj.skiAreas) !== 'undefined')
+    {
+        for (a in regionObj.skiAreas) {
+            
+            cnt += 1;
+            var skiArea = regionObj.skiAreas[a];
+            var closestPiste = Math.min(skiArea.closestDownhill, skiArea.closestNordic);
+            var closestLift = skiArea.closestLift;
+            
+            if (closestPiste / 1000 < close)
+                okPisteCnt +=1;
+            if (closestLift / 1000 < close)
+                okLiftCnt +=1;
+        }
+    }
+    for (elt in regionObj) 
+    {
+        if (typeof(regionObj.type) !== 'undefined') 
+        {
+            newCnt = countskiAreas(regionObj[elt]);
+            cnt += newCnt[0];
+            okPisteCnt += newCnt[1];
+            okLiftCnt += newCnt[2];
+        }
+        
+    }
+    
+    return [cnt, okPisteCnt, okLiftCnt]
+}
+
+function cntHTML(object) {
+        var newCnt = countskiAreas(object);
+        
+        var cnt = newCnt[0];
+        var okPisteCnt = newCnt[1];
+        var okLiftCnt = newCnt[2];
+        
+        var counter = document.createElement("div");
+        counter.classname = "counter";
+        
+        var values = document.createElement("span");
+        values.classname = "counterValues";
+        values.innerHTML = 'Out of ' + cnt.toString() + ' Skimaps.org ski areas, ' + okPisteCnt.toString() +' are close to OSM pistes, ' + okLiftCnt.toString() +' are close to OSM lifts';
+        
+        
+        var progress = document.createElement("progress");
+        progress.classname = "pistesProgress";
+        progress.value = okPisteCnt;
+        progress.max = cnt ;
+        
+        var progressLifts = document.createElement("progress");
+        progressLifts.classname = "liftProgress";
+        progressLifts.value = okLiftCnt;
+        progressLifts.max = cnt ;
+        
+        var ppc = document.createElement("span");
+        ppc.classname = "percent";
+        ppc.innerHTML = (okPisteCnt / cnt *100).toFixed().toString() + '% are close to OSM pistes';
+        
+        var lpc = document.createElement("span");
+        lpc.classname = "percent";
+        lpc.innerHTML = (okLiftCnt / cnt *100).toFixed().toString() + '% are close to OSM lifts';
+        
+        counter.append(values);
+        counter.append(document.createElement("br"));
+        counter.append(progress);
+        counter.append(ppc);
+        counter.append(progressLifts);
+        counter.append(lpc);
+        return counter
+}
+function buildHTML() {
+    
+    var summary = document.createElement("div");
+    var H1 = document.createElement("H1");
+    H1.innerHTML = 'Worldwide';
+    summary.append(H1);
+    
+    summary.append( cntHTML(dataReport) );
+    summary.className = "summary"
+    document.body.appendChild(summary);
+    
+    document.body.appendChild(document.createElement("hr"));
+    
+    for (var level1Id in dataReport)
+    {
+        if (dataReport[level1Id].name == null)
+            continue
+        var level1RegionElt = document.createElement("details");
+        level1RegionElt.className = "level1"
+        var level1RegionEltSum = document.createElement("summary");
+        var H1 = document.createElement("H1");
+        H1.innerHTML = dataReport[level1Id].name;
+        if (dataReport[level1Id].name == '-1')
+            H1.innerHTML = 'Not sorted';
+            
+        level1RegionEltSum.append(H1);
+        
+        level1RegionEltSum.append( cntHTML(dataReport[level1Id]) );
+        
+        level1RegionElt.appendChild(level1RegionEltSum);
+        document.body.appendChild(level1RegionElt);
+        
+        for (var level2Id in dataReport[level1Id])
+        {
+            if (level2Id != '-1' ){
+                if (dataReport[level1Id][level2Id].type == 'level2')
+                {
+
+                    var level2RegionElt = document.createElement("details");
+                    level2RegionElt.className = "level2"
+                    var level2RegionEltSum = document.createElement("summary");
+                    
+                    var H2 = document.createElement("H2");
+                    H2.innerHTML = dataReport[level1Id][level2Id].name;
+                    level2RegionEltSum.append(H2);
+                    
+                    var newCnt = countskiAreas(dataReport[level1Id][level2Id]);
+        
+                    level2RegionEltSum.append( cntHTML(dataReport[level1Id][level2Id]) );
+                    
+                    level2RegionElt.appendChild(level2RegionEltSum);
+                    
+                    level1RegionElt.appendChild(level2RegionElt);
+                    
+                    // scan for sublevel3 here
+                    for (var level3Id in dataReport[level1Id][level2Id])
+                    {
+                        if (dataReport[level1Id][level2Id][level3Id].type == 'level3')
+                        {
+                            var level3RegionElt = document.createElement("details");
+                            level3RegionElt.className = "level3"
+                            var level3RegionEltSum = document.createElement("summary");
+                            
+                            var H3 = document.createElement("H3");
+                            H3.innerHTML = dataReport[level1Id][level2Id][level3Id].name;
+                            level3RegionEltSum.append(H3);
+                        
+                            level3RegionEltSum.append( cntHTML(dataReport[level1Id][level2Id][level3Id]) );
+                            level3RegionElt.appendChild(level3RegionEltSum);
+                            
+                            level2RegionElt.appendChild(level3RegionElt);
+                            
+                            var skiAreas = dataReport[level1Id][level2Id][level3Id].skiAreas;
+                            populateSkiAreas(level3RegionElt, skiAreas);
+                        }
+                    }
+                }
+            }
+            else 
+            {
+                for (var level3Id in dataReport[level1Id]['-1'])
+                {
+                    if (dataReport[level1Id]['-1'][level3Id].type == 'level3')
+                    {
+                        var level3RegionElt = document.createElement("details");
+                        level3RegionElt.className = "level3"
+                        var level3RegionEltSum = document.createElement("summary");
+                    
+                        var H3 = document.createElement("H3");
+                        H3.innerHTML = dataReport[level1Id]['-1'][level3Id].name;
+                        level3RegionEltSum.append(H3);
+                        
+                            level3RegionEltSum.append( cntHTML(dataReport[level1Id]['-1'][level3Id]));
+                        level3RegionElt.appendChild(level3RegionEltSum);
+                        
+                        level1RegionElt.appendChild(level3RegionElt);
+                        var skiAreas = dataReport[level1Id][level2Id][level3Id].skiAreas;
+                        populateSkiAreas(level3RegionElt, skiAreas);
+                    }
+                }
+            }
+        }
+        
+    }
+}
+
+function populateSkiAreas(element, skiAreas) {
+    for (a in skiAreas) {
+        var skiArea = skiAreas[a];
+        var skiAreaElt = document.createElement("span");
+        skiAreaElt.className = "skiArea";
+        skiAreaElt.innerHTML = skiArea.name+'&nbsp;&nbsp;';
+        element.appendChild(skiAreaElt);
+        
+        var img1 = document.createElement('img');
+        img1.src='alpine-nb-20.png';
+        var text1 = document.createElement("span");
+        text1.innerHTML = (skiArea.closestDownhill / 1000).toFixed().toString()+'km&nbsp;';
+        text1.className = "distance";
+        var img2 = document.createElement('img');
+        img2.src='nordic-nb-20.png';
+        var text2 = document.createElement("span");
+        text2.innerHTML = (skiArea.closestNordic / 1000).toFixed().toString()+'km';
+        text2.className = "distance";
+        var img3 = document.createElement('img');
+        img3.src='drag_lift-nb-20.png';
+        var text3 = document.createElement("span");
+        text3.innerHTML = (skiArea.closestLift / 1000).toFixed().toString()+'km&nbsp;';
+        text3.className = "distance";
+        
+        if (skiArea.closestDownhill / 1000 <= close)
+            text1.style.backgroundColor="#6FFF3A";
+        else
+            text1.style.backgroundColor="#FF585C";
+        if (skiArea.closestNordic / 1000 <= close)
+            text2.style.backgroundColor="#6FFF3A";
+        else
+            text2.style.backgroundColor="#FF585C";
+        if (skiArea.closestLift / 1000 <= close)
+            text3.style.backgroundColor="#6FFF3A";
+        else
+            text3.style.backgroundColor="#FF585C";
+        
+        var link1=document.createElement('a');
+        link1.className = "ext_link";
+        link1.href='https://www.opensnowmap.org/?zoom=13&lat='+skiArea.lat+'&lon='+skiArea.lon;
+        link1.innerHTML = '&nbspOpensnowmap.org &nbsp';
+        link1.target="_blank";
+        
+        var link2=document.createElement('a');
+        link2.className = "ext_link";
+        link2.href='https://skimap.org/SkiAreas/view/'+skiArea.id;
+        link2.innerHTML = '&nbspSkimap.org &nbsp';
+        link2.target="_blank";
+        
+        var link3=document.createElement('a');
+        link3.className = "ext_link";
+        link3.href='https://www.openstreetmap.org/#map=13/'+skiArea.lat+'/'+skiArea.lon;
+        link3.innerHTML = '&nbspOpenStreetMap.org &nbsp';
+        link3.target="_blank";
+        
+            
+        element.appendChild(img1);
+        element.appendChild(text1);
+        element.appendChild(img2);
+        element.appendChild(text2);
+        element.appendChild(img3);
+        element.appendChild(text3);
+        element.appendChild(link1);
+        element.appendChild(link2);
+        element.appendChild(link3);
+        
+        var br = document.createElement("br");
+        element.appendChild(br);
+    }
+    return true;
+}
+
+function notes() {
+
+    
+    var xhr = [];
+    var n=0
+    for (var region_id in skimapsData)
+    {
+        var region = skimapsData[region_id];
+        for (var j = 0; j < region.length; j++)
+        {
+            (function(n){
+                var skiArea = region[j];
+                xhr[n] = new XMLHttpRequest();
+                var q = "http://www.opensnowmap.org/request?geo=true&list=true&closest=" + skiArea.lon + ',' + skiArea.lat;
+                xhr[n].open('GET', q, true);            
+                //~ xhr[n].timeout = 5000; 
+                
+                xhr[n].onreadystatechange = function () {
+                    if (xhr[n].readyState === 4  && xhr[n].status === 200) {
+                        var resp = xhr[n].responseText;
+                        //~ console.log(resp);
+                        var jsonPiste = JSON.parse(resp);
+                        //~ console.log(jsonPiste.snap.lon);
+                        
+                        var lonlat = {};
+                        lonlat.lon=parseFloat(skiArea.lon)
+                        lonlat.lat=parseFloat(skiArea.lat)
+                        
+                        var dist = distanceBetweenPoints(lonlat, jsonPiste.snap)/1000;
+                        color='PAleGreen';
+                        if (dist <= 10) { inOSM+=1;}
+                        if (dist > 2) { color = 'Salmon';}
+                        if (dist > 10) { color = 'Tomato';notinOSM+=1;}
+                        var text='<p style="background-color:'+ color +';">'+skiArea.name +': '+dist+' km - '+'in OSM : '+inOSM+' - '+'not in OSM : '+notinOSM+' - '+'ratio : '+notinOSM/inOSM*100+'%'+'</p>';
+                        console.log(text);
+                        list+=text;
+                        document.getElementById("list").innerHTML = list;
+                        return true;
+                    }
+                };
+                //~ xhr[n].ontimeout = function (e) { alert('Timeout')};
+                xhr[n].send(null);
+                
+            })(n);
+            n+=1;
+        }
+    }
+    
+}
